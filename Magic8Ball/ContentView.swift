6f3a5231adeb454118be249fbf02663734056920
Nell Brodkin <nellbrodkin@gmail.com>
2024-12-24 14:00:42 -0800

    Initial Commit

---
+++ Magic8Ball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magic8Ball
+//
+//  Created by nell on 12/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by nell on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
